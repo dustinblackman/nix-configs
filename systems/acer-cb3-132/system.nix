bdcdf7bb0c351cbb24732e4f05872f1b00d59f99
Dustin Blackman <dev@dustinblackman.com>
2020-12-30 13:41:51 -0500

    update light keys

--- systems/acer-cb3-132/system.nix
+++ systems/acer-cb3-132/system.nix
@@ -42,8 +42,8 @@ in
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -A 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -U 10"; }
+      { keys = [ 65 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -A 10"; }
+      { keys = [ 64 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -U 10"; }
     ];
   };
 
